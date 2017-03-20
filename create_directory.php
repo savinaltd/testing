e534619c833a22f56ffa9265108c2d57de65c6a7
savinaltd <joseph.savin@altd.in>
2017-03-20 13:15:29 +0530

    test commit

---
+++ create_directory.php
@@ -0,0 +1,14 @@
+<?php
+// Desired folder structure
+$structure = 'C:\\Users\\Administrator\\Documents\\Follow Liker\\Share\\savinaltd';
+
+// To create the nested structure, the $recursive parameter 
+// to mkdir() must be specified.
+
+if (!mkdir($structure, 0777, true)) {
+    die('Failed to create folders...');
+} else {
+	echo "success";
+}
+
+?>
